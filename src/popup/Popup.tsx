31f2d47f1aecc75b7b1f52230e7e90957792c7e9
Collin Town <35181633+towner-10@users.noreply.github.com>
2023-11-25 13:28:54 -0500

    Copy image to clipboard

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -4,17 +4,39 @@ import testImg from '../assets/test.png'
 
 import './Popup.css'
 
+async function copyToClipboard(pngBlob: Blob) {
+  try {
+    await navigator.clipboard.write([
+      new ClipboardItem({
+        [pngBlob.type]: pngBlob,
+      }),
+    ])
+    console.log('Image copied')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 export const Popup = () => {
+  const [pngBlob, setPngBlob] = useState<Blob>()
+
+  useEffect(() => {
+    fetch(testImg)
+      .then((res) => res.blob())
+      .then((blob) => setPngBlob(blob))
+  }, [])
 
   const handleCopy = () => {
-    // Copy image to clipboard
-  };
+    if (pngBlob) {
+      copyToClipboard(pngBlob)
+    }
+  }
 
   return (
     <main className="centered">
       <div className="header">
-        <img width="12%" src={logoAlt}/>
-        <h1>ChatMe–mseT</h1>
+        <img width="12%" src={logoAlt} />
+        <h1>ChatMe-meT</h1>
       </div>
       <label>Add the perfect meme response to your clipboard</label>
       <div className="gallery">
@@ -22,7 +44,7 @@ export const Popup = () => {
         <img src={testImg} onClick={handleCopy} />
       </div>
     </main>
-  );
+  )
 }
 
 export default Popup
