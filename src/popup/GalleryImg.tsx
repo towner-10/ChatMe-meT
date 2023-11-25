45dc638b60c0176470bb0adeb8b63b700671340c
Collin Town <35181633+towner-10@users.noreply.github.com>
2023-11-25 18:09:21 -0500

    Copy images from results to clipboard

--- src/popup/GalleryImg.tsx
+++ src/popup/GalleryImg.tsx
@@ -1,42 +1,60 @@
-import { useEffect, useState } from 'react';
-import './Popup.css';
-import testImg from '../assets/test.png'
+import { useEffect, useState } from 'react'
+import './Popup.css'
 
-async function copyToClipboard(pngBlob: Blob) {
-    try {
-      await navigator.clipboard.write([
-        new ClipboardItem({
-          [pngBlob.type]: pngBlob,
-        }),
-      ])
-      console.log('Image copied')
-    } catch (error) {
-      console.error(error)
+async function getCanvasBlob(src: string): Promise<Blob> {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.crossOrigin = 'Anonymous'
+    img.onload = () => {
+      const canvas = document.createElement('canvas')
+      canvas.width = img.width
+      canvas.height = img.height
+      const ctx = canvas.getContext('2d')
+      if (ctx) {
+        ctx.drawImage(img, 0, 0)
+        canvas.toBlob((blob) => {
+          if (blob) {
+            resolve(blob)
+          } else {
+            reject(new Error('Failed to get canvas blob'))
+          }
+        })
+      } else {
+        reject(new Error('Failed to get canvas context'))
+      }
+    }
+    img.src = src
+  })
+}
+
+async function copyToClipboard(src: string) {
+  console.log('Copying image to clipboard...')
+
+  try {
+    const pngBlob = await getCanvasBlob(src)
+    await navigator.clipboard.write([
+      new ClipboardItem({
+        [pngBlob.type]: pngBlob,
+      }),
+    ])
+    console.log('Image copied')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const GalleryImg = ({ image }: { image: string }) => {
+  const handleCopy = () => {
+    if (image) {
+      copyToClipboard(image)
     }
   }
 
-export const GalleryImg = ({ image }: {
-    image: string
-}) => {
-    const [pngBlob, setPngBlob] = useState<Blob>()
+  return (
+    <div className="img-wrapper">
+      <img src={image} onClick={handleCopy} />
+    </div>
+  )
+}
 
-    useEffect(() => {
-      fetch(image)
-        .then((res) => res.blob())
-        .then((blob) => setPngBlob(blob))
-    }, [])
-  
-    const handleCopy = () => {
-      if (pngBlob) {
-        copyToClipboard(pngBlob)
-      }
-    }
-
-    return (
-        <div className="img-wrapper">
-            <img src={image} onClick={handleCopy} />
-        </div>
-    );
-};
-
-export default GalleryImg;
+export default GalleryImg
