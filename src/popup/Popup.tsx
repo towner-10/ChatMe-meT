1166bb5df6504991242a43d1a5bbd3ccb307d350
Kevin Manka <kmanka@uwo.ca>
2023-11-25 12:51:18 -0500

    bad css

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,16 +1,26 @@
 import { useState, useEffect } from 'react'
 import logoAlt from '../assets/logo.png'
+import testImg from '../assets/test.png'
 
 import './Popup.css'
 
 export const Popup = () => {
+
+  const handleCopy = () => {
+    // Copy image to clipboard
+  };
+
   return (
     <main className="centered">
       <div className="header">
         <img width="12%" src={logoAlt}/>
-        <h1>ChatMe–meT</h1>
+        <h1>ChatMe–mseT</h1>
       </div>
       <label>Add the perfect meme response to your clipboard</label>
+      <div className="gallery">
+        <img src={testImg} onClick={handleCopy} />
+        <img src={testImg} onClick={handleCopy} />
+      </div>
     </main>
   );
 }
