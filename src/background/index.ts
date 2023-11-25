586a8ca30f09795dce48699b452d77a5ced54f33
yamaan93 <yamaan93@gmail.com>
2023-11-25 17:03:03 -0500

    add functions for extracting conversation context and getting search terms

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,13 @@
+import { updateContext } from "../lib/context/context"
+
 console.log('background is running')
 
 chrome.runtime.onMessage.addListener((request) => {
   if (request.type === 'COUNT') {
     console.log('background has received a message from popup, and count is ', request?.count)
+  }
+  else if (request.type === 'MESSAGE') {
+    console.log('background has received a message from contentScript')
+    updateContext(request);
   }
 })
